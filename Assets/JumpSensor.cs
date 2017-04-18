7d971feb8d0743d805b5e26fe43cc275db6f9b40
Jian-Bin Liu <billyljb@gmail.com>
2017-04-18 13:14:13 +0800

    update Jump

---
+++ Assets/JumpSensor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSensor : MonoBehaviour {
+
+    private int TouchedCount = 0;
+
+	void OnTriggerEnter(Collider other)
+    {
+        TouchedCount++;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        TouchedCount--;
+    }
+
+    public bool IsCanJump()
+    {
+        return TouchedCount > 0;
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,23 +7,59 @@ public class PlayerController : MonoBehaviour {
     public Transform rotateXTransform;
     public float rotateSpeed;
     public float currentRotateX = 0;
+    public float MoveSpeed;
+    float currentSpeed = 0;
+
+    public Rigidbody rigidBody;
+
+    public JumpSensor JumpSensor;
+    public float JumpSpeed;
+
     // Use this for initialization
     void Start()
     {
         animatorController = this.GetComponent<Animator>();
     }
-    public float MoveSpeed;
-    float currentSpeed = 0;
+
     // Update is called once per frame
     void Update()
     {
-        float result = 0;
-        if (Input.GetKey(KeyCode.W)) {
-            result += MoveSpeed;
+        //決定鍵盤input的結果
+        Vector3 movDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) { movDirection.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movDirection.z -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movDirection.x += 1; }
+        if (Input.GetKey(KeyCode.A)) { movDirection.x -= 1; }
+        movDirection = movDirection.normalized;
+
+        //決定要給Animator的動畫參數
+        if (movDirection.magnitude == 0 || !JumpSensor.IsCanJump()) {
+            currentSpeed = 0;
+        } 
+        else {
+            if (movDirection.z < 0) {
+                currentSpeed = -MoveSpeed;
+            } 
+            else {
+                currentSpeed = MoveSpeed;
+            }
         }
-        if (Input.GetKey(KeyCode.S)) {
-            result -= MoveSpeed;
+        animatorController.SetFloat("Speed", currentSpeed);
+
+        //轉換成世界座標的方向
+        Vector3 worldSpaceDirection = movDirection.z * rotateYTransform.transform.forward +
+                                      movDirection.x * rotateYTransform.transform.right;
+        Vector3 velocity = rigidBody.velocity;
+        velocity.x = worldSpaceDirection.x * MoveSpeed;
+        velocity.z = worldSpaceDirection.z * MoveSpeed;
+
+        if(Input.GetKey(KeyCode.Space) && JumpSensor.IsCanJump()) {
+            velocity.y = JumpSpeed;
         }
+
+        rigidBody.velocity = velocity;
+
+        //計算滑鼠
         rotateYTransform.transform.localEulerAngles += new Vector3(0, Input.GetAxis("Horizontal"), 0) * rotateSpeed;
         currentRotateX += Input.GetAxis("Vertical") * rotateSpeed;
 
@@ -33,11 +69,6 @@ public class PlayerController : MonoBehaviour {
             currentRotateX = -90;
         }
         rotateXTransform.transform.localEulerAngles = new Vector3(-currentRotateX, 0, 0);
-        currentSpeed = result;
-
-        rotateYTransform.transform.position += Time.deltaTime * currentSpeed * rotateYTransform.transform.forward;
-
-        animatorController.SetFloat("Speed", currentSpeed);
 
     }
 }
