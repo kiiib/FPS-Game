6d2b610943cb51d53d1158dd169b8ede6b5b5c6a
Jian-Bin Liu <billyljb@gmail.com>
2017-04-18 16:39:02 +0800

    update bullet

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float FlyingSpeed;
+    public float LifeTime;
+
+    public void InitAndShot(Vector3 Direction)
+    {
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody.velocity = Direction * FlyingSpeed;
+        Invoke("KillYourSelf", LifeTime);
+    }
+
+    public void KillYourSelf() { 
+
+        GameObject.Destroy(this.gameObject);
+    }
+    
+    void OnTriggerEnter(Collider other) {
+        KillYourSelf();
+    }
+}
